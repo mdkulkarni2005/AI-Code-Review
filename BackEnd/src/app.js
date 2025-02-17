b1a95af2fd0ab5484e85cbd2ba5bba92446ac04f
mdkulkarni2005 <147185935+mdkulkarni2005@users.noreply.github.com>
2025-02-17 09:47:01 +0530

    Add Google Gemini API key to environment configuration

--- BackEnd/src/app.js
+++ BackEnd/src/app.js
@@ -1,9 +1,14 @@
 const express = require('express');
+const aiRoute = require('./routes/ai.routes');
+
+
 
 const app = express();
 
 app.get('/', (req, res) => {
   res.send('Hello World');
 })
+
+app.use('/ai', aiRoute);
 
 module.exports = app
